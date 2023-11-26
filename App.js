44367264f15fb61873627a0c9950c0820d2a2c73
BunjanMark <bunjan.mark476@gmail.com>
2023-11-26 20:40:06 +0800

    login UI/ transition complete

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 function HomeScreen() {
   return (
@@ -21,7 +22,11 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,32 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Button, PaperProvider, TextInput, Text } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
+  const navigator = useNavigation();
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <PaperProvider>
+      <SafeAreaView>
+        <Text>Log in</Text>
+        <TextInput mode="outlined" label={"Username"} />
+        <TextInput mode="outlined" label={"Password"} />
+
+        <Button>Log in</Button>
+        <SafeAreaView>
+          <Text>
+            Not a member?{" "}
+            <Button
+              onPress={() => {
+                navigator.navigate("RegisterScreen");
+              }}
+            >
+              Sign up now
+            </Button>
+          </Text>
+        </SafeAreaView>
+      </SafeAreaView>
+    </PaperProvider>
   );
 };
 
